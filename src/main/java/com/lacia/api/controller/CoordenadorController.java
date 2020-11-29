3be1826ae90ec3289759b077566fabf1c16adfa5
Diego Ribeiro <diegoaribeiro1910@gmail.com>
2020-11-29 16:14:39 -0300

    Adição do crossOrigin

--- src/main/java/com/lacia/api/controller/CoordenadorController.java
+++ src/main/java/com/lacia/api/controller/CoordenadorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class CoordenadorController {
 		this.coordenadorService = coordenadorService;
 	}
 	
-	
+	@CrossOrigin
 	@GetMapping("/coordenadores")
 	public ResponseEntity<Object> obterTodosCoordenadores(@RequestHeader String Authorization) {
 		try {
