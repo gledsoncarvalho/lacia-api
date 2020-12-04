55aa28616a0d7069c9c7a01f44a44242f39f76da
Gledson <gledsoncarvalho99@hotmail.com>
2020-12-04 12:58:40 -0300

    implementa EtiquetaRepository

---
+++ src/main/java/com/lacia/api/repository/EtiquetaRepository.java
@@ -0,0 +1,9 @@
+package com.lacia.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lacia.api.model.Etiqueta;
+
+public interface EtiquetaRepository extends JpaRepository<Etiqueta, Integer> {
+
+}
